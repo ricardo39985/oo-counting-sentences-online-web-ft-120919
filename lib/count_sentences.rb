607f08fbc0ebab70bf4700fe3a80e13653178f0e
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-19 15:12:16 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self[-1]=='.'? true : false
   end
 
   def question?
-
+    return self[-1]=='?'? true : false
   end
 
   def exclamation?
-
+    return self[-1]=='!'? true : false
   end
 
   def count_sentences
-
+    self.split('.').flatten.map {|s|s.split('!')}.flatten.map {|w|w.split('?').flatten}.length
   end
 end
